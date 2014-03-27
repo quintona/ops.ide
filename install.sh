61efac174a9896fb236d39a5fed05d74ff6b2bfd
Quinton Anderson <quintona@gmail.com>
2014-03-27 20:55:35 +1100

    added install

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+sudo yum -y install npm git
+
--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 
-sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo yum -y install npm git
 git clone https://github.com/exsilium/cloud9.git
 cd cloud9
-git branch terminal
+git checkout terminal
 npm install
 mkdir ~/workspace
