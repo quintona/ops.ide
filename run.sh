e79ef8b201e7a6f7e7f39c3ade1103d123b9e64f
Quinton Anderson <quintona@gmail.com>
2014-03-27 20:51:39 +1100

    initial commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 for port in $(seq 8800 8900); 
-do echo -ne "\035" | telnet 127.0.0.1 $port > /dev/null 2>&1; [ $? -eq 1 ] && echo "Running ide on $port" && ~/cloud9/bin/cloud9.sh --l 0.0.0.0 --p=$port && break; done
+do echo -ne "\035" | telnet 127.0.0.1 $port > /dev/null 2>&1; [ $? -eq 1 ] && echo "Running ide on $port" && ~/cloud9/bin/cloud9.sh --w ./workspace/%1 --l 0.0.0.0 --p=$port && break; done
--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
 
 sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo yum install npm git
+sudo yum -y install npm git
 git clone https://github.com/exsilium/cloud9.git
 cd cloud9
 git branch terminal
 npm install
 mkdir ~/workspace
- 
